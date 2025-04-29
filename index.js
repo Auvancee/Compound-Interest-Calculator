12d693cf169566bfc12e84eb6495a21314541927
Aakif Umar <therealauvance@gmail.com>
2025-04-28 20:14:43 -0700

    - Created a compoundInterest function to calculate final investment value with compounding.
    - Created a calculateRegular function to calculate
    total without compounding.
    
    - Built a functionRun to prompt user inputs
    (initial amount, monthly contribution, years,
    interest rate).
    
    - Added printOutput function to display a
    detailed summary of inputs and results.
    
    - Connected all functions and ran the full
    program flow.
    
    - Installed package.json with prompt-sync

---
+++ index.js
@@ -0,0 +1,59 @@
+// Compound Interest Calculator that prompts a user for some inputs
+// Finally calculates the compounded interest value
+
+let initialAmount = 20000
+let monthlyContribution = 400
+let numberOfYears = 30
+let interestRate = 10
+
+// [1] define function to calculate final value of compound interest
+const compoundInterest = (initialAmount, monthlyContribution, numberOfYears, interestRate) => {
+    let total = initialAmount
+    let annualContribution = monthlyContribution * 12
+
+    for (let i = 0; i < numberOfYears; i++) {
+        total = total + annualContribution
+        total = total * (100 + interestRate) / 100
+    }
+
+    return total.toFixed(2)
+}
+
+// calculating regular amount
+const calculateRegular = (initialAmount, monthlyContribution, numberOfYears) => {
+    return (initialAmount + monthlyContribution * 12 * numberOfYears).toFixed(2)
+}
+
+
+// [2] Run function that prompts user to enter all necessary details required to calculate the final amounts
+const functionRun = () =>  {
+    let initialAmount = prompt('What is your initial investment ($)? ')
+    let monthlyContribution = prompt('What is your monthly contribution ($)? ')
+    let numberOfYears = prompt('For how many years would you like to compound your investment? ')
+    let interestRate = prompt('What is your expected interest rate (%) over these years? ')
+
+    printOutput(initialAmount, monthlyContribution, numberOfYears, interestRate)
+}
+
+
+// test run
+// console.log(compoundInterest(initialAmount, monthlyContribution, numberOfYears, interestRate) 
+// - calculateRegular(initialAmount, monthlyContribution, numberOfYears))
+
+// printing the output
+const printOutput = (initialAmount, monthlyContribution, numberOfYears, interestRate) => {
+
+    let finalValue = compoundInterest(initialAmount, monthlyContribution, numberOfYears, interestRate) // with interest rate
+    let valueWithoutCompounding = calculateRegular(initialAmount, monthlyContribution, numberOfYears) // without interest rate
+
+    let summary = `INIT_AMOUNT: ${initialAmount}\nMONTHLY_CONTRIBUTION: ${monthlyContribution}\n
+    NUMBER_OF_YEARS: ${numberOfYears}\nINTEREST_RATE: ${interestRate}\n\nFINAL_COMPOUNDED_VALUE: 
+    $${finalValue}\nREGULAR_AMOUNT: $${valueWithoutCompounding}\nDIFFERENCE: $${finalValue - valueWithoutCompounding}`
+
+    console.log(summary)
+}
+
+
+
+functionRun()
+
